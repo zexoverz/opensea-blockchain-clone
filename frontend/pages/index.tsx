055bb8953b1df4ced9f494a97c47fc9e25c1fafa
MID_Dev <aryuical@gmail.com>
2022-02-21 22:50:08 +0700

    Header Initiallize

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,10 +1,11 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import Header from '../components/Header'
+
+
 
 const Home: NextPage = () => {
   return (
-    <h1>Hello World</h1>
+    <Header/>
   )
 }
 

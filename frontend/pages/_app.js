dc17250be1f25b71f56cf22ab3aeb56ea794f170
MID_Dev <aryuical@gmail.com>
2022-02-26 22:33:20 +0700

    Install thirdweb hooks

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,4 +1,10 @@
 import '../styles/globals.css'
+import {ThirdwebWeb3Provider} from '@3rdweb/hooks'
+
+const supportedChainIds = [4]
+const connectors = {
+  injected: {}
+}
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />

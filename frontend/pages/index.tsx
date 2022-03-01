ef5a486110552423e5f8f51001c5f2860c06e66a
MID_Dev <aryuical@gmail.com>
2022-03-01 17:00:27 +0700

    install hot toaster lib

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -4,6 +4,7 @@ import Hero from '../components/Hero'
 import {useWeb3} from '@3rdweb/hooks'
 import {useEffect} from 'react'
 import {client} from '../lib/sanityClient'
+import toast, {Toaster} from 'react-hot-toast'
  
 
 const style = {
@@ -11,7 +12,7 @@ const style = {
   walletConnectWrapper: `flex flex-col justify-center items-center h-screen w-screen bg-[#3b3d42] `,
   button: `border border-[#282b2f] bg-[#2081e2] p-[0.8rem] text-xl font-semibold rounded-lg cursor-pointer text-black`,
   details: `text-lg text-center text=[#282b2f] font-semibold mt-4`,
-}
+} 
 
 
 const Home: NextPage = () => {

75fd2a40994d45b92921da70786c0f9aac7aa28f
MID_Dev <aryuical@gmail.com>
2022-03-02 11:10:10 +0700

    Create NFT Module and fetch all Nfts

--- frontend/pages/collections/[collectionId].js
+++ frontend/pages/collections/[collectionId].js
@@ -1,7 +1,31 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useMemo} from 'react'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
 import { useWeb3 } from '@3rdweb/hooks'
+import { ThirdwebSDK } from '@3rdweb/sdk'
+
+
+const style = {
+  bannerImageContainer: `h-[20vh] w-screen overflow-hidden flex justify-center items-center`,
+  bannerImage: `w-full object-cover`,
+  infoContainer: `w-screen px-4`,
+  midRow: `w-full flex justify-center text-white`,
+  endRow: `w-full flex justify-end text-white`,
+  profileImg: `w-40 h-40 object-cover rounded-full border-2 border-[#202225] mt-[-4rem]`,
+  socialIconsContainer: `flex text-3xl mb-[-2rem]`,
+  socialIconsWrapper: `w-44`,
+  socialIconsContent: `flex container justify-between text-[1.4rem] border-2 rounded-lg px-2`,
+  socialIcon: `my-2`,
+  divider: `border-r-2`,
+  title: `text-5xl font-bold mb-4`,
+  createdBy: `text-lg mb-4`,
+  statsContainer: `w-[44vw] flex justify-between py-4 border border-[#151b22] rounded-xl mb-4`,
+  collectionStat: `w-1/4`,
+  statValue: `text-3xl font-bold w-full flex items-center justify-center`,
+  ethLogo: `h-6 mr-2`,
+  statName: `text-lg w-full text-center mt-1`,
+  description: `text-[#8a939b] text-xl w-max-1/4 flex-wrap mt-4`,
+}
 
 
 const Collection = () => {
@@ -12,29 +36,28 @@ const Collection = () => {
   const [nfts, setNfts] = useState([])
   const [listings, setListings] = useState([])
 
+  const nftModule = useMemo(() => {
+    if(!provider) return
 
-  const style = {
-    bannerImageContainer: `h-[20vh] w-screen overflow-hidden flex justify-center items-center`,
-    bannerImage: `w-full object-cover`,
-    infoContainer: `w-screen px-4`,
-    midRow: `w-full flex justify-center text-white`,
-    endRow: `w-full flex justify-end text-white`,
-    profileImg: `w-40 h-40 object-cover rounded-full border-2 border-[#202225] mt-[-4rem]`,
-    socialIconsContainer: `flex text-3xl mb-[-2rem]`,
-    socialIconsWrapper: `w-44`,
-    socialIconsContent: `flex container justify-between text-[1.4rem] border-2 rounded-lg px-2`,
-    socialIcon: `my-2`,
-    divider: `border-r-2`,
-    title: `text-5xl font-bold mb-4`,
-    createdBy: `text-lg mb-4`,
-    statsContainer: `w-[44vw] flex justify-between py-4 border border-[#151b22] rounded-xl mb-4`,
-    collectionStat: `w-1/4`,
-    statValue: `text-3xl font-bold w-full flex items-center justify-center`,
-    ethLogo: `h-6 mr-2`,
-    statName: `text-lg w-full text-center mt-1`,
-    description: `text-[#8a939b] text-xl w-max-1/4 flex-wrap mt-4`,
-  }
+    const sdk = new ThirdwebSDK(
+      provider.getSigner(),
+      'https://eth-rinkeby.alchemyapi.io/v2/7xTTO9NMW8awh-bYeIWgXO9syXGXT4Pu'
+    )
+    
+    return sdk.getNFTModule(collectionId)
+  }, [provider])
 
+  // get all NFTs in the collection 
+  useEffect(() => {
+    if(!nftModule) return
+
+    ;(async () => {
+      const nfts = await nftModule.getAll()
+
+      setNfts(nfts)
+    })()
+  }, [nftModule])
+  
   return (
     <Link href="/">
         <h2>{router.query.collectionId}</h2>
